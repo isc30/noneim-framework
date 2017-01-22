c1d1ce95c77c0977fd63aff0ee53782b57559c01
Ivan Sanz-Carasa <me@ivansanz.com>
2017-01-22 23:18:22 +0100

    [#22] Fix documentation script

--- generateDocumentation.sh
+++ generateDocumentation.sh
@@ -1,2 +1,2 @@
 rm -rf ~/workspace/doc/*
-apigen generate -s ~/workspace -d ~/workspace/doc --access-levels="public,protected" --template-theme bootstrap --tree --title "DAM Final Project"
+apigen generate -s ~/workspace -d ~/workspace/doc --access-levels="public" --template-theme bootstrap --tree --title "PHP-Framework"
