3e36a64ad13822bfb3a3a9993d2846a0b812e6af
Ivan Sanz <ivansanzcarasa@gmail.com>
2017-01-23 19:11:18 +0100

    [#22] Refactor WebConfiguration

--- Solution/Modules/Web/WebConfiguration.php
+++ Solution/Modules/Web/WebConfiguration.php
@@ -1,10 +1,8 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: black
- * Date: 14/01/2017
- * Time: 15:25
+ * Default Web Module Configuration
+ * @package Modules\Web
  */
 class WebConfiguration implements IConfiguration
 {
