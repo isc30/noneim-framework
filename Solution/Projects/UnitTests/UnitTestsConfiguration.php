9cdaae7e018b365f2bbfbe5e165d8431dcb6f50b
Ivan Sanz <ivansanzcarasa@gmail.com>
2017-01-23 19:45:16 +0100

    [#22] Refactor Project Configurations

--- Solution/Projects/UnitTests/UnitTestsConfiguration.php
+++ Solution/Projects/UnitTests/UnitTestsConfiguration.php
@@ -1,10 +1,8 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: black
- * Date: 22/01/2017
- * Time: 1:36
+ * UnitTests Project Configuration
+ * @package UnitTests
  */
 class UnitTestsConfiguration implements IProjectLazyConfiguration
 {
