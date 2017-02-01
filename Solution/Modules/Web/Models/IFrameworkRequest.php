9bff773dfbf82fe90acb1c616680ab6727cb94f6
Ivan Sanz-Carasa <me@ivansanz.com>
2017-02-01 20:24:38 +0100

    [#32] Introduce WebRequestService

--- Solution/Modules/Web/Models/IFrameworkRequest.php
+++ Solution/Modules/Web/Models/IFrameworkRequest.php
@@ -5,26 +5,10 @@
  */
 class IFrameworkRequest
 {
+    /** @var string */
     public $section;
+    /** @var IFrameworkRequestParameters */
     public $parameters;
-
-    public function __construct()
-    {
-        $this->section = $this->getSection();
-        $this->parameters = new IFrameworkRequestParameters($_GET, $_POST);
-    }
-
-    private function getSection()
-    {
-        if (isset($_GET[WebConfiguration::$sectionRequest]))
-        {
-            $section = $_GET[WebConfiguration::$sectionRequest];
-            if ($section !== '')
-            {
-                return $section;
-            }
-        }
-
-        return WebConfiguration::$defaultSection;
-    }
+    /** @var string */
+    public $type;
 }
--- Solution/Modules/Web/Models/IFrameworkRequestParameters.php
+++ Solution/Modules/Web/Models/IFrameworkRequestParameters.php
@@ -5,58 +5,92 @@
  */
 class IFrameworkRequestParameters
 {
-    private $get;
+    /** @var string[] */
+    private $_get;
+    /** @var string[] */
     private $post;
 
+    /**
+     * IFrameworkRequestParameters Constructor
+     * @param string[] $get
+     * @param string[] $post
+     */
     public function __construct($get, $post)
     {
-        $this->get = $get;
+        $this->_get = $get;
         $this->post = $post;
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public function existsGet($key)
     {
-        return isset($this->get[$key]);
+        return isset($this->_get[$key]);
     }
 
+    /**
+     * @param string $key
+     * @return null|string
+     */
     public function get($key)
     {
-        if (!$this->existsGet($key)) return null;
-        return $this->get[$key];
+        if ($this->existsGet($key))
+        {
+            return $this->_get[$key];
+        }
+
+        return null;
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public function existsPost($key)
     {
         return isset($this->post[$key]);
     }
 
+    /**
+     * @param string $key
+     * @return null|string
+     */
     public function post($key)
     {
-        if (!$this->existsPost($key)) return null;
-        return $this->post[$key];
+        if ($this->existsPost($key))
+        {
+            return $this->post[$key];
+        }
+
+        return null;
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public function exists($key)
     {
         return $this->existsGet($key) || $this->existsPost($key);
     }
 
+    /**
+     * @param string $key
+     * @return null|string
+     */
     public function any($key)
     {
         if ($this->existsPost($key))
         {
             return $this->post($key);
         }
-        else
+        elseif ($this->existsGet($key))
         {
-            if ($this->existsGet($key))
-            {
-                return $this->get($key);
-            }
-            else
-            {
-                return null;
-            }
+            return $this->get($key);
         }
+
+        return null;
     }
 }
