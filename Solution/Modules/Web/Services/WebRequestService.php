7ec73922ad35e8f809bf0aa25b03f5df6aa85e03
Ivan Sanz <ivansanzcarasa@gmail.com>
2017-02-01 20:24:38 +0100

    [#32] Introduce WebRequestService

--- Solution/Modules/Web/Services/WebRequestService.php
+++ Solution/Modules/Web/Services/WebRequestService.php
@@ -10,6 +10,40 @@ class WebRequestService implements IWebRequestService
      */
     public function getCurrent()
     {
-        return new IFrameworkRequest();
+        $request = new IFrameworkRequest();
+        $request->parameters = new IFrameworkRequestParameters($_GET, $_POST);
+        $request->section = $this->getSection($request->parameters);
+        $request->type = $this->getType();
+
+        return $request;
+    }
+
+    /**
+     * @param IFrameworkRequestParameters $parameters
+     * @return string
+     */
+    private function getSection(IFrameworkRequestParameters $parameters)
+    {
+        $section = $parameters->get(WebConfiguration::$sectionRequest);
+
+        if (!ValidationHelper::isNullOrEmpty($section))
+        {
+            return $section;
+        }
+
+        return WebConfiguration::$defaultSection;
+    }
+
+    /**
+     * @return string
+     */
+    private function getType()
+    {
+        if (isset($_SERVER['REQUEST_METHOD']))
+        {
+            return $_SERVER['REQUEST_METHOD'];
+        }
+
+        return RequestType::Get;
     }
 }
